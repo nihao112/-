246e568291b96d7acb5d0bb6c5099802f678ac11
nihao <hao15710065945@163.com>
2021-11-08 09:52:43 +0800

    八哥修复

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -15,30 +15,46 @@
       </div>
 
     </div> -->
-    <div style='margin-left:40px;margin-top:10px'><img src="6.png" alt="" style='width:100px'></div>
-    <el-menu class="sidebar-el-menu" :default-active="onRoutes" :collapse="collapse" background-color="#f6f6f6"
-      text-color="#bfcbd9" active-text-color="#20a0ff" unique-opened router>
+    <div style='margin-left:40px;margin-top:10px'><img src="6.png"
+           alt=""
+           style='width:100px'></div>
+    <el-menu class="sidebar-el-menu"
+             :default-active="onRoutes"
+             :collapse="collapse"
+             background-color="#f6f6f6"
+             text-color="#bfcbd9"
+             active-text-color="#20a0ff"
+             unique-opened
+             router>
       <template v-for="item in items">
         <template v-if="item.subs">
-          <el-submenu :index="item.index" :key="item.index">
+          <el-submenu :index="item.index"
+                      :key="item.index">
             <template #title>
               <i :class="item.icon"></i>
               <span>{{ item.title }}</span>
             </template>
             <template v-for="subItem in item.subs">
-              <el-submenu v-if="subItem.subs" :index="subItem.index" :key="subItem.index">
+              <el-submenu v-if="subItem.subs"
+                          :index="subItem.index"
+                          :key="subItem.index">
                 <template #title>{{ subItem.title }}</template>
-                <el-menu-item v-for="(threeItem, i) in subItem.subs" :key="i" :index="threeItem.index">
+                <el-menu-item v-for="(threeItem, i) in subItem.subs"
+                              :key="i"
+                              :index="threeItem.index">
                   {{ threeItem.title }}</el-menu-item>
               </el-submenu>
-              <el-menu-item v-else :index="subItem.index" :key="subItem.index">{{ subItem.title }}
+              <el-menu-item v-else
+                            :index="subItem.index"
+                            :key="subItem.index">{{ subItem.title }}
               </el-menu-item>
             </template>
           </el-submenu>
 
         </template>
         <template v-else>
-          <el-menu-item :index="item.index" :key="item.index">
+          <el-menu-item :index="item.index"
+                        :key="item.index">
             <i :class="item.icon"></i>
             <template #title>{{ item.title }}</template>
           </el-menu-item>
